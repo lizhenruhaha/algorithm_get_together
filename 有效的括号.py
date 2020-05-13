48d37e0c7f799fb2e2d21cdba0e97b021e930415
lizhenruhaha <2134933571@qq.com>
2020-05-13 18:19:30 +0800

    update

---
+++ 有效的括号.py
@@ -0,0 +1,64 @@
+# _*_coding: utf-8 _*_
+__author__ = 'anroy'
+__data__ = '2020-05 -13 :17:39'
+
+'''
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
+
+
+'''
+
+# 示例
+'''
+示例 1:
+
+输入: "()"
+输出: true
+示例 2:
+
+输入: "()[]{}"
+输出: true
+示例 3:
+
+输入: "(]"
+输出: false
+示例 4:
+
+输入: "([)]"
+输出: false
+示例 5:
+
+输入: "{[]}"
+输出: true
+
+'''
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {')': '(', ']': '[', '}': '{'}
+        stack = []
+        for i in s:
+            print('asdfg',i)
+            if stack and i in dic:
+                if stack[-1] == dic[i]:stack.pop()
+                else:return False
+            else:
+                stack.append(i)
+            print(stack)
+        return not stack
+
+obj=Solution()
+print(obj.isValid('(]'))
+
+
+
+
+
+
